23cd1e740a63f7700c50cba8dda525f51f9d715a
juan cruz <jcrnietos@gmail.com>
2025-05-07 16:17:33 -0300

    cambios en configuracion de base de datos

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,19 +1,37 @@
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME_PROD as string,
-  process.env.DB_USER_PRO as string,
-  process.env.DB_PASSWORD_PROD,
-  {
-    host: process.env.DB_HOST_PROD,
-    dialect: 'postgres',
-    port: parseInt(process.env.DB_PORT_PROD || '5432'),
-    logging: false,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME_PROD as string,
+//   process.env.DB_USER_PRO as string,
+//   process.env.DB_PASSWORD_PROD,
+//   {
+//     host: process.env.DB_HOST_PROD,
+//     dialect: 'postgres',
+//     port: parseInt(process.env.DB_PORT_PROD || '5432'),
+//     logging: false,
+//   }
+// );
 
-export default sequelize;
+// export default sequelize;
 
+const connectionString = process.env.DATABASE_URL
+const sequelize = connectionString
+  ? new Sequelize(connectionString, {
+      dialect: 'postgres',
+      dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME_PROD as string,
+      process.env.DB_USER_PROD as string,
+      process.env.DB_PASSWORD_PROD as string,
+      {
+        host: process.env.DB_HOST_PROD,
+        port: parseInt(process.env.DB_PORT_PROD || '5432'),
+        dialect: 'postgres',
+        logging: false,
+      }
+    )
